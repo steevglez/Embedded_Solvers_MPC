ee5a457282283b3d5d939083158dda38be5b8593
steevglez <steev.gonzalez95@gmail.com>
2022-03-28 01:13:53 -0300

    FiOrdOS

--- OSQP-FIXED_ITER-N4/source/mmultSW.h
+++
@@ -1,9 +0,0 @@
-#ifndef MMULTSW_H
-#define MMULTSW_H
-
-#include "specs.h"
-#include <cblas.h>
-
-void mmultSW (elem *A, CBLAS_TRANSPOSE tansposeA, elem *B, CBLAS_TRANSPOSE tansposeB, elem *C, int m, int n, int p);
-
-#endif
