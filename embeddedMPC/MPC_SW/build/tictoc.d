442b44d766d63d48a161a5d4990f3127eb8010ac
steevglez <61519714+steevglez@users.noreply.github.com>
2022-03-28 00:47:18 -0300

    Delete embeddedMPC/MPC_SW directory

--- embeddedMPC/MPC_SW/build/tictoc.d
+++
@@ -1,5 +0,0 @@
-build/tictoc.o: source/tictoc.cpp source/tictoc.h source/specs.h
-
-source/tictoc.h:
-
-source/specs.h:
--- embeddedMPC/MPC_SW/source/qpoasesSW.h
+++
@@ -1,8 +0,0 @@
-#ifndef QPOASESSW_H
-#define QPOASESSW_H
-
-#include <qpOASES.hpp>
-
-void qpoasesSW (elem H[N_QP *N_QP], elem h[N_QP], elem M[I_QP*N_QP], elem c[I_QP], elem tk[N_QP]);
-
-#endif // QPOASESSW_H
